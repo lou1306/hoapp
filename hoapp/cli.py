2cc517e1f0a24b964bcd24eeae435c3b9b8e4f7b
Luca Di Stefano <luca.di.stefano@tuwien.ac.at>
2025-07-11 14:49:09 +0200

    Documentation, CLI usage messages

--- hoapp/cli.py
+++ hoapp/cli.py
@@ -8,10 +8,10 @@ from typing import Annotated, List, Optional
 import typer
 
 import hoapp.util as util
+import hoapp.strings as strings
 from hoapp.ast.ast import Type
 from hoapp.util import filt
 from hoapp.util import product as prod
-
 from .parser import parse
 
 main = typer.Typer(pretty_exceptions_show_locals=False)
@@ -19,12 +19,12 @@ main = typer.Typer(pretty_exceptions_show_locals=False)
 
 @main.command()
 def version():
-    """Print version information and exit"""
+    """Print version information and exit."""
     print(f"HOApp v{importlib.metadata.version("hoapp")}")
 
 
 def catch_errors(debug: bool):
-    """_summary_
+    """Reusable decorator to handle printing/hiding of errors.
 
     Args:
         debug (bool): Indicates whether the user set the `--debug` flag.
@@ -47,10 +47,10 @@ def catch_errors(debug: bool):
 
 @main.command()
 def check(
-    filename: Annotated[Path, typer.Argument(help="Path to a HOApp automaton.")],  # noqa: E501
-    debug: Annotated[bool, typer.Option(help="Add debug information.")] = False
+    filename: Annotated[Path, typer.Argument(help=strings.hoapp_path_help)],
+    debug: Annotated[bool, typer.Option(help=strings.debug_help)] = False
 ):
-    """Parse and type-check a HOApp automaton"""
+    """Parse and type-check a HOApp automaton."""
     def fn():
         aut = parse(filename)
         aut.type_check()
@@ -67,11 +67,11 @@ def check(
 
 @main.command()
 def autfilt(
-    filename: Annotated[Path, typer.Argument(help="Path to a HOApp automaton.")],  # noqa: E501
+    filename: Annotated[Path, typer.Argument(help=strings.hoapp_path_help)],
     args: List[str],
-    debug: Annotated[bool, typer.Option(help="Add debug information.")] = False
+    debug: Annotated[bool, typer.Option(help=strings.debug_help)] = False
 ):
-    """Wrap Spot's autfilt"""
+    """Wrap Spot's autfilt."""
 
     def fn():
         aut = parse(filename)
@@ -82,11 +82,11 @@ def autfilt(
 
 @main.command()
 def product(
-    filename1: Annotated[Path, typer.Argument(help="Path to a HOApp automaton.")],  # noqa: E501
-    filename2: Annotated[Path, typer.Argument(help="Path to a HOApp automaton.")],  # noqa: E501
-    debug: Annotated[bool, typer.Option(help="Add debug information.")] = False
+    filename1: Annotated[Path, typer.Argument(help=strings.hoapp_path_help)],
+    filename2: Annotated[Path, typer.Argument(help=strings.hoapp_path_help)],
+    debug: Annotated[bool, typer.Option(help=strings.debug_help)] = False
 ):
-    """Compute the product of two hoapp automata (if it exists)"""
+    """Compute the product of two hoapp automata (if it exists)."""
     def fn():
         aut1, aut2 = parse(filename1), parse(filename2)
         aut = prod(aut1, aut2)
@@ -97,10 +97,26 @@ def product(
 
 @main.command()
 def ltl2tgba(
-    formula: Annotated[str, typer.Option("--formula", "-f")],
-    ap_type: Annotated[Optional[List[str]], typer.Option("--type", "-t")] = None,  # noqa: E501
-    debug: Annotated[bool, typer.Option(help="Add debug information.")] = False
+    formula: Annotated[str, typer.Option("--formula", "-f", help=strings.formula_help)],  # noqa: E501
+    ap_type: Annotated[Optional[List[str]], typer.Option("--type", "-t", help=strings.type_help)] = None,  # noqa: E501
+    debug: Annotated[bool, typer.Option(help=strings.debug_help)] = False
 ):
+    """Translate LTL formulas into a HOApp Büchi automaton.
+
+    The formula may contain atoms which are arithmetic expressions over integer
+    or real variables.
+    Types for all non-Boolean variables must be provided with --type/-t.
+
+    Use ALIASES for variables and i/r PREFIXES for literals.
+    For instance, consider the formula "Globally, variable a is equal to 1":
+
+    * G(a == 1)         # WRONG, no quotes
+
+    * G("a == 1")       # WRONG, did not use aliases and prefixes
+
+    * G("@a == i1")     # CORRECT
+    """
+
     def parse_cli_type(cli_type: str) -> tuple[str, Type]:
         name, typ = cli_type.split("=")
         return name.strip(), Type(typ.strip())
---
+++ hoapp/strings.py
@@ -0,0 +1,6 @@
+debug_help: str = "Print additional debug information."
+formula_help: str = "LTL formula to translate."
+hoapp_path_help: str = "Path to a HOApp automaton."
+type_help: str = (
+    "Type information for proposition. Format: <variable>=<bool|int|real>. "
+    "May be specified multiple times.")
