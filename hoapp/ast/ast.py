88b4b1a592a2e015e89289a8ba7e65421d285a69
Luca Di Stefano <luca.di.stefano@tuwien.ac.at>
2025-07-30 15:19:25 +0200

    Add LTL type

--- hoapp/ast/ast.py
+++ hoapp/ast/ast.py
@@ -6,12 +6,15 @@ class Type(Enum):
     BOOL = "bool"
     INT = "int"
     REAL = "real"
+    LTL = "ltl"
 
     def __le__(self, other) -> bool:
         if not isinstance(other, Type):
             return False
         if self == Type.INT:
             return other in (Type.REAL, Type.INT)
+        if self == Type.BOOL:
+            return other in (Type.BOOL, Type.LTL)
         return self == other
 
 
